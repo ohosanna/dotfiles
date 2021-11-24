a0a35c11793d622025c600cecfebd03fb68eb833
hosanna <ohosanna@gmail.com>
2021-11-24 21:54:32 +0800

    fish 配置更新

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,9 @@ alias h='history'
 alias tailf='tail -f'
 
 # Fisher init
-if not functions -q fisher; eval (curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher); end
+#if not functions -q fisher; eval (curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher); end
 #fisher update
+set --universal nvm_default_version lts
+set --universal nvm_mirror https://npm.taobao.org/mirrors/node
+nvm use lts default
 
