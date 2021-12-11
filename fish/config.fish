a1cd0cc6b68de2641d0398fc88c1132cfe86df3e
ohosanna <ohosanna@gmail.com>
2021-12-11 10:24:08 +0800

    nvim 配置更新，增加yabai配置

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,8 @@ alias v='vim'
 alias l='ls -lvh'
 alias h='history'
 alias tailf='tail -f'
+alias python='python3'
+alias vim='nvim'
 
 # Fisher init
 if not functions -q fisher; eval (curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher); end
